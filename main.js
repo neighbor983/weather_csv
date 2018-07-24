488020331d97192cb93945d956dcc4be7734433f
neighbor983 <kenneth.ray.baker@gmail.com>
2018-07-24 05:38:17 +0000

    can make the api call. need to make sure what data we want and then add it to a csv

--- main.js
+++ main.js
@@ -6,8 +6,30 @@ const DARKSKY_API_KEY =config.darkSkyApiKey;
 const latitude = config.latitude;
 const longitude = config.longitude;
 
-axios.get(BASE_URL + DARKSKY_API_KEY + '/' + latitude + ',' + longitude)
-  .then(function(response) {
-    console.log('data',response.data);
-    console.log('status',response.status);
-  });
+//24 hr/day * 60 min/day * 60 secs/min * 1000 sec/msec 
+const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+
+let startDateString ='2017-01-01';
+const startDate = new Date(startDateString).getTime();
+
+let endDateString ='2017-01-03';
+const endDate= new Date(endDateString).getTime();
+
+let timeCounter = startDate;
+
+do {
+    console.log('timeCounter:',timeCounter);
+    timeCounter = timeCounter + MILLISECONDS_PER_DAY;
+    
+    /*
+    axios.get(BASE_URL + DARKSKY_API_KEY + '/' + latitude 
+              + ',' + longitude + ',' + timeCounter + 
+              '?exclude=currently,minutely,hourly,flags')
+      .then(function(response) {
+        console.log('data',response.data);
+      }
+    );
+  */
+  
+}
+while( timeCounter <= endDate);
