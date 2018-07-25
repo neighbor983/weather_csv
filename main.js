90f42ba14ea561dce32a16d90b6b5878c20c25ee
neighbor983 <kenneth.ray.baker@gmail.com>
2018-07-25 05:14:25 +0000

    creates a csv from a date range

--- main.js
+++ main.js
@@ -1,35 +1,135 @@
 const axios = require("axios");
 const config = require('./config');
+const fs = require('fs');
+const Json2csvParser = require('json2csv').Parser;
+
+let startDateString = '2018-07-22';
+let endDateString = '2018-07-23';
+
+const NEW_LINE = "\r\n";
+const startDate = new Date(startDateString);
+const endDate = new Date(endDateString);
+const fileName = 'WeatherData' + startDateString + 'to' + endDateString 
+                  + 'at' + new Date().getTime() + '.csv';
+const fields = [
+  "time",
+  "summary",
+  "icon",
+  "sunriseTime",
+  "sunsetTime",
+  "moonPhase",
+  "precipIntensity",
+  "precipIntensityMax",
+  "precipIntensityMaxTime",
+  "precipProbability",
+  "precipType",
+  "temperatureHigh",
+  "temperatureHighTime",
+  "temperatureLow",
+  "temperatureLowTime",
+  "apparentTemperatureHigh",
+  "apparentTemperatureHighTime",
+  "apparentTemperatureLow",
+  "apparentTemperatureLowTime",
+  "dewPoint",
+  "humidity",
+  "pressure",
+  "windSpeed",
+  "windGust",
+  "windGustTime",
+  "windBearing",
+  "cloudCover",
+  "uvIndex",
+  "uvIndexTime",
+  "visibility",
+  "ozone",
+  "temperatureMin",
+  "temperatureMinTime",
+  "temperatureMax",
+  "temperatureMaxTime",
+  "apparentTemperatureMin",
+  "apparentTemperatureMinTime",
+  "apparentTemperatureMax",
+  "apparentTemperatureMaxTime"
+];
+
+const opts = { fields, header: false };
+
+fs.writeFile(fileName, fields + NEW_LINE, 'utf8', function(err) {
+  if (err) {
+    console.log('Some error occured - file either not saved or corrupted file saved.');
+  } else {
+    console.log('It\'s saved!');
+  }
+});
 
 const BASE_URL = 'https://api.darksky.net/forecast/';
-const DARKSKY_API_KEY =config.darkSkyApiKey;
+const DARKSKY_API_KEY = config.darkSkyApiKey;
 const latitude = config.latitude;
 const longitude = config.longitude;
 
-//24 hr/day * 60 min/day * 60 secs/min * 1000 sec/msec 
-const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
-
-let startDateString ='2017-01-01';
-const startDate = new Date(startDateString).getTime();
-
-let endDateString ='2017-01-03';
-const endDate= new Date(endDateString).getTime();
-
+const parser = new Json2csvParser(opts);
 let timeCounter = startDate;
 
 do {
-    console.log('timeCounter:',timeCounter);
-    timeCounter = timeCounter + MILLISECONDS_PER_DAY;
-    
-    /*
-    axios.get(BASE_URL + DARKSKY_API_KEY + '/' + latitude 
-              + ',' + longitude + ',' + timeCounter + 
-              '?exclude=currently,minutely,hourly,flags')
-      .then(function(response) {
-        console.log('data',response.data);
-      }
-    );
-  */
-  
+  timeCounter = addDays(timeCounter, 1);
+  let timeString = convertTimeStamp(timeCounter);
+
+  axios.get(BASE_URL + DARKSKY_API_KEY + '/' + latitude +
+      ',' + longitude + ',' + timeString +
+      '?exclude=currently,minutely,hourly,flags')
+    .then(function(response) {
+      addDailyWeatherRecord(response.data, parser, NEW_LINE);
+    });
 }
-while( timeCounter <= endDate);
+while (timeCounter <= endDate);
+
+function addDays(beginDate, numberOfDays) {
+  let resultDate = new Date(
+    beginDate.getFullYear(),
+    beginDate.getMonth(),
+    beginDate.getDate() + numberOfDays,
+    beginDate.getHours(),
+    beginDate.getMinutes(),
+    beginDate.getSeconds()
+  );
+  return resultDate;
+}
+
+function convertMonth(monthValue) {
+  let newMonth = monthValue + 1;
+  if (newMonth < 10) {
+    return '0' + newMonth;
+  }
+  else {
+    return newMonth;
+  }
+}
+
+function convertDay(dayValue) {
+  if (dayValue < 10) {
+    return '0' + dayValue;
+  }
+  else {
+    return dayValue;
+  }
+}
+
+function convertTimeStamp(dateTimeStamp) {
+  let timeStamp = dateTimeStamp.getFullYear() + '-' +
+    convertMonth(dateTimeStamp.getMonth()) + '-' +
+    convertDay(dateTimeStamp.getDate()) +
+    'T00:00:00';
+
+  return timeStamp;
+}
+
+function addDailyWeatherRecord(data, parser, NEW_LINE){
+  let locationInformation = data;
+  let dailyWeather = locationInformation.daily.data[0];
+
+  fs.appendFile(fileName, parser.parse(dailyWeather) + NEW_LINE, function(err) {
+    if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+  });
+}
